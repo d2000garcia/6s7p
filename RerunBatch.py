7fbf291985ae216796160197673f3ef3ecc40673
Diego Garcia <d2000garcia@gmail.com>
2026-07-13 21:56:57 -0400

    Ready to run all, fit Gamma vs not gamma

--- RerunBatch.py
+++ RerunBatch.py
@@ -12,11 +12,11 @@ from numpy import pi as pi
 from scipy.special import wofz as wofz
 
 def check_for_analysis(folder,F1,check_against=[]):
-    # laser = '894'
-    if os.getlogin() == 'garci868':
-            laser = '894'
-    else:
-        laser = '456'
+    laser = '456'
+    # if os.getlogin() == 'garci868':
+    #         laser = '894'
+    # else:
+    #     laser = '456'
     temp = os.listdir(path = folder)
     if 'Analysis' in temp:
         if not (True in list(map(lambda y: y in folder, check_against))):
@@ -49,25 +49,29 @@ def redo_analysis(par_folder,F1):
         # if not os.path.exists(par_folder+'\\redone.txt'):
             #we've fit before
         print(par_folder)
-        if os.getlogin() == 'garci868':
-            laser = '894'
-        else:
-            laser = '456'
+        # if os.getlogin() == 'garci868':
+        #     laser = '894'
+        # else:
+        #     laser = '456'
         # file = open(par_folder+'\\redone2.txt','w+')
         # file.close()
-        if laser == '456':
-            scan = Absorption_calc.data(par_folder,exists=True)
-            scan.F1=F1
-            scan.set_transition(F1=F1)
-        else:
-            scan = Absorption_calc.data(par_folder,scan='894',exists=True)
-        scan.calculate_beat_fit()
-        scan.set_fitting_function()
+        to_do = ['456','894']
+        for laser in to_do:
+            if laser == '456':
+                scan = Absorption_calc.data(par_folder,exists=True)
+                scan.F1=F1
+                scan.set_transition(F1=F1)
+            else:
+                scan = Absorption_calc.data(par_folder,scan='894',exists=True)
+            scan.calculate_beat_fit()
+            scan.set_fitting_function()
     
 base_dir = os.getcwd()
 
 base_folder = base_dir+ r'\BeatnotePostHotCell\Jun16,2026'
 F1 = 3
+folders = [r'\BeatnotePostHotCell',r'\BeatnotePostHotCellF1=4']
+F1s = [3,4]
 
 # if os.getlogin() == 'garci868':
 #     base_folder = base_dir+ r'\BeatnotePostHotCell\Jun16,2026'
@@ -76,4 +80,5 @@ F1 = 3
 #     base_folder = base_dir+ r'\BeatnotePostHotCellF1=4'
 #     F1 = 4
 if __name__ == '__main__':
-    check_for_analysis(base_folder,F1)
+    check_for_analysis(base_dir+folders[0],F1s[0])
+    check_for_analysis(base_dir+folders[1],F1s[1])
