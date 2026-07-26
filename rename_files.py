29d4ce64a46b1fbc9428b28d52e738f44efc52e9
Diego Garcia <d2000garcia@gmail.com>
2026-07-25 21:57:29 -0400

    rm fixed gamma

--- rename_files.py
+++ rename_files.py
@@ -4,22 +4,25 @@ def check_for_analysis(folder):
     name_change = 'Old894'
     dir_lst = os.listdir(folder)
     x = list(os.scandir(folder))
-    temp = list(map(lambda y:'894Fitparams' in y,dir_lst))
+    temp = list(map(lambda y:'456Fitparams' in y,dir_lst))
     if True in temp:
-        # place456 = temp.index(True)
-        # name_456 = dir_lst[place456]
+        place456 = temp.index(True)
+        name_456 = dir_lst[place456]
+        if 'nonfixed' in name_456:
+            name_456 = dir_lst[place456].replace('_nonfixed_gamma','')
+            os.rename(folder+'\\'+dir_lst[place456],folder+'\\'+name_456)
         # name_456 = name_456[:name_456.find('.tsv')] + name_change + name_456[name_456.find('.tsv'):]
         # os.rename(folder+'\\'+dir_lst[place456],folder+'\\'+name_456)
 
-        place894 = list(map(lambda y:'894Fitparams' in y,dir_lst)).index(True)
-        name_894 = dir_lst[place894].replace('894Fitparams','Old894')
-        # name_894 = name_894[:name_894.find('.tsv')] + name_change + name_894[name_894.find('.tsv'):]
-        os.rename(folder+'\\'+dir_lst[place894],folder+'\\'+name_894)
+        # place894 = list(map(lambda y:'894Fitparams' in y,dir_lst)).index(True)
+        # name_894 = dir_lst[place894].replace('894Fitparams','Old894')
+        # # name_894 = name_894[:name_894.find('.tsv')] + name_change + name_894[name_894.find('.tsv'):]
+        # os.rename(folder+'\\'+dir_lst[place894],folder+'\\'+name_894)
     else:
         for val in x:
             if val.is_dir():
                 check_for_analysis(val.path)
 if __name__ == '__main__':
     base_dir = os.getcwd()
-    start_folder = base_dir+r'\BeatnotePostHotCellF1=4'
+    start_folder = base_dir+r'\BeatnotePostHotCell'
     check_for_analysis(start_folder)
