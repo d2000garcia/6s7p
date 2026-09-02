6f24d40428b2a546bca3421385180b6d80bc3aad
Diego Garcia <d2000garcia@gmail.com>
2026-09-02 18:31:56 -0400

    Linear measurement calc

---
+++ GrabVoltageAvgs.py
@@ -0,0 +1,43 @@
+import os as os
+import numpy as np
+
+def check_for_analysis(folder,Background):
+    global pulledDat
+    dat456 = {}
+    dat894 = {}
+    dir_lst = os.listdir(folder)
+    x = list(os.scandir(folder))
+    temp = list(map(lambda y:'456ScanV2.csv' in y,dir_lst))
+    if True in temp:
+        signals = np.loadtxt(folder+'\\456ScanV2.csv',delimiter=',',dtype=float)
+        sig = [np.mean(signals[:,0:5])-Background[0],np.mean(signals[:,5:10])-Background[1],np.mean(signals[:,10:15])-Background[2]]
+        sig.extend([np.std(signals[:,0:5],ddof=1),np.std(signals[:,5:10],ddof=1),np.std(signals[:,10:15],ddof=1)])
+        if len(pulledDat) != 0:
+            notdone = True
+            for i, num in enumerate(pulledDat):
+                if sig[0] > num[0] and notdone:
+                    notdone = False
+                    pulledDat.insert(i,sig)
+            if notdone:
+                pulledDat.insert(0,sig)
+        else:
+            pulledDat.append(sig)
+
+    else:
+        for val in x:
+            if val.is_dir():
+                check_for_analysis(val.path,Background)
+
+
+base_dir = os.getcwd()
+global pulledDat
+pulledDat = []
+
+start_folder = base_dir + '\\LinMeas\\Aug31,2026'
+if __name__ == '__main__':
+    loaded = np.loadtxt(start_folder+'\\Background.csv',delimiter=',',dtype=float)
+    background = np.mean(loaded,0)
+    # file = open(start_folder+'\\PulledDat.csv','w')
+    # file.close()
+    check_for_analysis(start_folder,background)
+    np.savetxt(start_folder+'\\PulledDat.csv',pulledDat,'%.5f',delimiter=',')
